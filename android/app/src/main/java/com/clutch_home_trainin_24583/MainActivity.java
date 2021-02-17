f9b7592c2a923a0fb2346602cb41b032f58b507c
Crowdbotics <team@crowdbotics.com>
2021-02-17 19:15:58 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/clutch_home_trainin_24583/MainActivity.java
@@ -0,0 +1,15 @@
+package com.clutch_home_trainin_24583;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "clutch_home_trainin_24583";
+    }
+}
